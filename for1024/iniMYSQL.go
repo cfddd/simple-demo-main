542dddf3a89e159cfe2d579d209b1a801837512a
cfd <2909047212@qq.com>
2023-08-30 10:38:47 +0800

    修复喜欢total_favorite的bug 更新for1024文件夹，有所有1024平台需要的可执行文件

--- for1024/iniMYSQL.go
+++ for1024/iniMYSQL.go
@@ -5,18 +5,20 @@ import (
 	"github.com/RaymondCode/simple-demo/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
+	"strconv"
 )
 
 var DB *gorm.DB
 
 func Init() {
 
-	username := "root"   //用户名
-	password := "123456" //密码
-	host := "127.0.0.1"  //数据库地址，可以是IP或者域名
-	port := 3306         //端口号
-	Dbname := "douyin"   //数据库名
-	timeout := "10s"     //超时连接，10秒
+	username := os.Getenv("MYSQL_USER")                          //用户名
+	password := os.Getenv("MYSQL_PASSWORD")                      //密码
+	host := os.Getenv("MYSQL_HOST")                              //数据库地址，可以是IP或者域名
+	port, _ := strconv.ParseInt(os.Getenv("MYSQL_PORT"), 10, 32) //端口号
+	Dbname := "douyin"                                           //数据库名
+	timeout := "10s"                                             //超时连接，10秒
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
 
